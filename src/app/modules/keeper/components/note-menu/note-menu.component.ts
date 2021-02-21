875941b7044c098b887a1bc02ace897d818e0c4f
vinh <fangireking789@gmail.com>
2021-02-21 22:01:51 +0700

    2nd:

--- src/app/modules/keeper/components/note-menu/note-menu.component.ts
+++ src/app/modules/keeper/components/note-menu/note-menu.component.ts
@@ -1,230 +1,249 @@
-import { Component, OnInit, Input, ViewChild, EventEmitter, Output } from '@angular/core';
-import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatAutocompleteModule, MatAutocompleteTrigger } from '@angular/material/autocomplete';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { LabelService } from '../../../shared/services/label.service';
-import { Note } from '../../../../models/note-model';
-import { DialogueConfirmComponent } from '../../../shared/components/dialogue-confirm/dialogue-confirm.component';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { NoteService } from '../../../shared/services/note.service';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  EventEmitter,
+  Output,
+} from "@angular/core";
+import { MatMenuModule, MatMenuTrigger } from "@angular/material/menu";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import {
+  MatAutocompleteModule,
+  MatAutocompleteTrigger,
+} from "@angular/material/autocomplete";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { LabelService } from "../../../shared/services/label.service";
+import { Note } from "../../../../models/note-model";
+import { DialogueConfirmComponent } from "../../../shared/components/dialogue-confirm/dialogue-confirm.component";
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+} from "@angular/material/dialog";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { NoteService } from "../../../shared/services/note.service";
 
 @Component({
-  selector: 'app-note-menu',
-  templateUrl: './note-menu.component.html',
-  styleUrls: ['./note-menu.component.css']
+  selector: "app-note-menu",
+  templateUrl: "./note-menu.component.html",
+  styleUrls: ["./note-menu.component.css"],
 })
 export class NoteMenuComponent implements OnInit {
   @Input() note: Note;
   @Input() details: boolean; // means that menu not in noteComponent
   @Input() newNote: boolean; // means that menu in newPageComponent
-  selecetdFile : File;
+  selecetdFile: File;
   menuActive: boolean = false; // means that one of menu item open
   labelList: string[];
 
   minDate: Date = new Date();
   maxDate: Date = new Date(2030, 0, 1);
 
-  colors:string[] = [     // Available menu colors
-    '#fefefe',
-    '#f28c80',
-    '#f6bf02',
-    '#fff478',
-    '#cdfe91',
-    '#a6fdea',
-    '#cdeff8',
-    '#afcbfa',
-    '#d5affc',
-    '#fdcfe9',
-    '#e2cba9',
-    '#e9eaee'
+  colors: string[] = [
+    // Available menu colors
+    "#fefefe",
+    "#f28c80",
+    "#f6bf02",
+    "#fff478",
+    "#cdfe91",
+    "#a6fdea",
+    "#cdeff8",
+    "#afcbfa",
+    "#d5affc",
+    "#fdcfe9",
+    "#e2cba9",
+    "#e9eaee",
   ];
 
-
-  constructor(private noteService: NoteService,
-              private labelService: LabelService,
-              public dialog: MatDialog,
-              private snackBar: MatSnackBar) {
+  constructor(
+    private noteService: NoteService,
+    private labelService: LabelService,
+    public dialog: MatDialog,
+    private snackBar: MatSnackBar
+  ) {
     this.labelList = labelService.getLabels();
   }
 
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 
   // Image upload event handler
-  onFileUpload(event){
-
+  onFileUpload(event) {
     this.selecetdFile = event.target.files[0];
     const reader = new FileReader();
 
     // Set uploading start/end handlers
     let setFileProgress = this.setFileProgress.bind(this);
-    reader.onloadstart = (function(){
+    reader.onloadstart = function () {
       setFileProgress(true);
-    });
-    reader.onloadend = (function(){
+    };
+    reader.onloadend = function () {
       // Load emulation
-      setTimeout( () => {
+      setTimeout(() => {
         setFileProgress(false);
       }, 3000);
-    });
+    };
 
     // Set file to note
     reader.onload = () => {
       this.note.imagePreview = reader.result as string;
     };
     reader.readAsDataURL(this.selecetdFile);
-
   }
 
   // Outputing file upload start/end event
   @Output() fileProgress = new EventEmitter<boolean>();
-  setFileProgress(show:boolean) {
-          this.fileProgress.emit(show);
+  setFileProgress(show: boolean) {
+    this.fileProgress.emit(show);
   }
 
   // Check current color is selected
-  isSelect(index:number):boolean{
-    return (this.note.selectedColor==index);
+  isSelect(index: number): boolean {
+    return this.note.selectedColor == index;
   }
   // Color menu item click event handler
-  colorClick(index:number){
-      this.note.selectedColor=index;
-      this.note.color = this.colors[index];
+  colorClick(index: number) {
+    this.note.selectedColor = index;
+    this.note.color = this.colors[index];
   }
 
-  getColor(index:number):string{
+  getColor(index: number): string {
     return this.colors[index];
   }
 
   // Toggle label menu item checkbox
-  changeCheck(index, $event:any){
+  changeCheck(index, $event: any) {
     let labelIndexOf = this.note.labels.indexOf(this.labelList[index]);
-    if (labelIndexOf != -1){
+    if (labelIndexOf != -1) {
       this.note.labels.splice(labelIndexOf, 1);
-    }
-    else this.note.labels.push(this.labelList[index]);
+    } else this.note.labels.push(this.labelList[index]);
   }
 
   // Check current label menu item is active
-  labelChecked(index):boolean{
+  labelChecked(index): boolean {
     if (this.note.labels.indexOf(this.labelList[index]) != -1) return true;
     else return false;
   }
 
   // Set selected time to date
-  updateTime(time){
-    if((this.note.date!=undefined)&&(this.note.date!=null)){
-      if (!time)  time = '00:00';
+  updateTime(time) {
+    if (this.note.date != undefined && this.note.date != null) {
+      if (!time) time = "00:00";
 
-      let timeArray = time.split(':');
-      this.note.date = new Date(this.note.date.getFullYear(),
-                                this.note.date.getMonth(),
-                                this.note.date.getDate(),
-                                timeArray[0], timeArray[1], 0);
+      let timeArray = time.split(":");
+      this.note.date = new Date(
+        this.note.date.getFullYear(),
+        this.note.date.getMonth(),
+        this.note.date.getDate(),
+        timeArray[0],
+        timeArray[1],
+        0
+      );
     }
   }
 
   // Delete or restore note
   deleteConfirmDialogue(): void {
-    if(this.newNote){
+    if (this.newNote) {
       this.deleteNote();
-      this.snackBar.open('Заметка перемещена в корзину', null, {duration: 2000});
-    }
-    else if(!this.note.trash){
-      const dialogRef = this.dialog.open(DialogueConfirmComponent, {
-        width: '250px',
-        data: 'Удалить заметку?'
+      this.snackBar.open("Đã chuyển ghi chú vào thùng rác", null, {
+        duration: 2000,
       });
-      dialogRef.afterClosed().subscribe(result => {
-        if (result){
+    } else if (!this.note.trash) {
+      const dialogRef = this.dialog.open(DialogueConfirmComponent, {
+        width: "250px",
+        data: "Удалить заметку?",
+      });
+      dialogRef.afterClosed().subscribe((result) => {
+        if (result) {
           this.deleteNote();
-          this.snackBar.open('Заметка перемещена в корзину', null, {duration: 2000});
+          this.snackBar.open("Đã chuyển ghi chú vào thùng rác", null, {
+            duration: 2000,
+          });
         }
       });
-    }
-    else {
+    } else {
       this.deleteNote();
-      this.snackBar.open('Заметка восстановлена', null, {duration: 2000});
+      this.snackBar.open("Đã khôi phục ghi chú", null, { duration: 2000 });
     }
     this.deleteNoteEmit();
   }
 
-  deleteNote(){
+  deleteNote() {
     this.note.arhieved = false;
     this.note.trash = !this.note.trash;
   }
 
   @Output() noteDelete = new EventEmitter<boolean>();
-  deleteNoteEmit(){
+  deleteNoteEmit() {
     this.noteDelete.emit(true);
   }
 
-
   // Arhive the note
-  arhiveClick(){
+  arhiveClick() {
     this.note.trash = false;
     this.note.arhieved = !this.note.arhieved;
-    if (this.note.arhieved) this.snackBar.open('Заметка перемещена в архив', null, {duration: 2000});
-    else this.snackBar.open('Заметка восстановлена из архива', null, {duration: 2000});
+    if (this.note.arhieved)
+      this.snackBar.open("Đã chuyển ghi chú vào kho lưu trữ", null, {
+        duration: 2000,
+      });
+    else
+      this.snackBar.open("Đã khôi phục ghi chú từ kho lưu trữ", null, {
+        duration: 2000,
+      });
     this.archiveNoteEmit();
   }
 
   @Output() noteArhive = new EventEmitter<boolean>();
-   archiveNoteEmit(){
-     this.noteArhive.emit(true);
-   }
+  archiveNoteEmit() {
+    this.noteArhive.emit(true);
+  }
 
   // Showed note edit todo list
-  showTodoList(){
-    if(this.details){
+  showTodoList() {
+    if (this.details) {
       this.note.showTodo = true;
       this.showTodo();
-    }
-    else{
+    } else {
       this.note.showTodo = !this.note.showTodo;
     }
-
   }
 
   // Outputing todoList menu click
-  @Output() showNote = new EventEmitter<boolean>()
+  @Output() showNote = new EventEmitter<boolean>();
   showTodo() {
-        this.showNote.emit(true);
-    }
-
+    this.showNote.emit(true);
+  }
 
   // Outputing menu opened trigger
   @Output() setMenu = new EventEmitter<boolean>();
-  setMenuStatus(status:boolean) {
+  setMenuStatus(status: boolean) {
     this.setMenu.emit(status);
   }
 
   // Fix closing autocomplete by clicking outside
   @ViewChild(MatAutocompleteTrigger) autocomplete: MatAutocompleteTrigger;
   @ViewChild("auto") myautocomplete;
-  hideAutoComplete(event){
-      if((this.myautocomplete.isOpen)&&(!event.target.closest('.timeField'))){
-        this.autocomplete.closePanel();
-      }
+  hideAutoComplete(event) {
+    if (this.myautocomplete.isOpen && !event.target.closest(".timeField")) {
+      this.autocomplete.closePanel();
+    }
   }
 
   // Closes menu on close click
-  @ViewChild('alarmMenuTrigger') alarmMenuTrigger : MatMenuTrigger;
-  @ViewChild('labelMenuTrigger') labelMenuTrigger : MatMenuTrigger;
-  closeMyMenu(){
+  @ViewChild("alarmMenuTrigger") alarmMenuTrigger: MatMenuTrigger;
+  @ViewChild("labelMenuTrigger") labelMenuTrigger: MatMenuTrigger;
+  closeMyMenu() {
     this.alarmMenuTrigger.closeMenu();
     this.labelMenuTrigger.closeMenu();
   }
 
   //
-  showMoreMenuItem(){
-    if ( this.newNote && this.noteService.checkNoteIsEmpty(this.note) ) return false;
+  showMoreMenuItem() {
+    if (this.newNote && this.noteService.checkNoteIsEmpty(this.note))
+      return false;
     else return true;
   }
-
-
 }
--- src/app/modules/shared/services/label.service.ts
+++ src/app/modules/shared/services/label.service.ts
@@ -1,51 +1,51 @@
-import { Injectable } from '@angular/core';
-import { NoteService } from './note.service';
+import { Injectable } from "@angular/core";
+import { NoteService } from "./note.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class LabelService {
-  private labels:string[];
+  private labels: string[];
 
   constructor(private noteService: NoteService) {
-    this.labels = ['Источники вдохновения', 'Личный', 'Рабочий', 'Важное', 'Черновик'];
+    this.labels = [
+      "xem phim",
+      "đánh thằng duy",
+      "đi bar",
+      "mua xe",
+      "nhắn cho gái ",
+    ];
   }
 
-
-  getLabels():string[]{
+  getLabels(): string[] {
     return this.labels;
   }
 
   // Check label name is unique
-  verifyUnique(newLabel:string):boolean{
+  verifyUnique(newLabel: string): boolean {
     newLabel = this.transformString(newLabel);
-    if (this.labels.indexOf(newLabel)!=-1) return false;
+    if (this.labels.indexOf(newLabel) != -1) return false;
     else return true;
   }
 
-  pushLabel(newLabel:string){
-    this.labels.push( this.transformString(newLabel) );
+  pushLabel(newLabel: string) {
+    this.labels.push(this.transformString(newLabel));
   }
 
-  deleteLabel(label:string){
+  deleteLabel(label: string) {
     this.labels.splice(this.labels.indexOf(label), 1);
     this.noteService.destroyLabel(label);
   }
 
-  renameLabel(newLabel:string, oldLabel:string){
+  renameLabel(newLabel: string, oldLabel: string) {
     newLabel = this.transformString(newLabel);
     this.labels[this.labels.indexOf(oldLabel)] = newLabel;
     this.noteService.renameLabel(newLabel, oldLabel);
   }
 
   // Format label string
-  transformString(str:string):string {
+  transformString(str: string): string {
     if (!str) return str;
     return (str[0].toUpperCase() + str.slice(1).toLowerCase()).trim();
   }
-
-
-
-
-
 }
