e6da404289f6126d483379db207a47db2f32de35
vinh <fangireking789@gmail.com>
2021-02-21 22:17:10 +0700

    fd

--- src/app/modules/shared/services/url-naming.service.ts
+++ src/app/modules/shared/services/url-naming.service.ts
@@ -1,41 +1,37 @@
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class UrlNamingService {
-
-  constructor(public router: Router) { }
+  constructor(public router: Router) {}
 
   // Return current url name
-  getRouteName():string{
+  getRouteName(): string {
+    let route = decodeURI(this.router.url);
+    if (route.indexOf("/label/") != -1) {
+      return route.replace("/label/", "");
+    } else {
+      route = route.replace("/", "");
 
-   let route = decodeURI(this.router.url);
-   if (route.indexOf('/label/')!=-1){
-     return route.replace('/label/', '');
-   }
-   else{
-     route = route.replace('/', '');
-
-     switch(route) {
-        case 'notes':
-          return 'Keeper'
+      switch (route) {
+        case "notes":
+          return "Keeper";
           break;
-        case 'alarms':
-          return 'Оповещения'
+        case "alarms":
+          return "Cảnh báo";
           break;
-        case 'arhive':
-          return 'Архив'
+        case "arhive":
+          return "Lưu trữ";
           break;
-        case 'trash':
-          return 'Корзина'
+        case "trash":
+          return "Thùng rác";
           break;
         default:
-          return 'Keeper'
+          return "Keeper";
           break;
       }
-   }
-
+    }
   }
 }
